c1e062f383a047749be4af45f500cbb200f5bdef
hmustain <mustain05@aim.com>
2025-03-10 18:43:33 -0500

    updated racer model

--- server/models/Racer.js
+++ server/models/Racer.js
@@ -1,8 +1,11 @@
+const mongoose = require("mongoose");
+
 const RacerSchema = new mongoose.Schema({
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
-    points: { type: Number, default: 0 },
-    heats: [{ type: mongoose.Schema.Types.ObjectId, ref: "Heat" }],
-    seed: { type: Number, default: null }
-  });
-  
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  points: { type: Number, default: 0 }, // Total points from heat races
+  heats: [{ type: mongoose.Schema.Types.ObjectId, ref: "Heat" }], // Track assigned heats
+  seed: { type: Number, default: null } // Seeding for the bracket
+});
+
+module.exports = mongoose.model("Racer", RacerSchema);
