286c79be45726575f7408acc3d831246c96d003c
hmustain <mustain05@aim.com>
2025-03-10 20:37:47 -0500

    updated for testing 2

--- server/seed/seedRandomScoring.js
+++ server/seed/seedRandomScoring.js
@@ -9,21 +9,20 @@ async function seedRandomScoring() {
     
     for (const heat of heats) {
       const numRacers = heat.racers.length;
-      // Generate placements array and shuffle it
+      // Create an array of placements [1, 2, ..., numRacers] and shuffle it
       let placements = Array.from({ length: numRacers }, (_, i) => i + 1);
       placements.sort(() => Math.random() - 0.5);
-
-      // Create results array mapping each racer to a random placement
+      
+      // For each racer in the heat, assign a random placement
       const results = heat.racers.map((racerId, index) => ({
-        racer: racerId,
+        racer: racerId, // This should be a valid ObjectId from the racers array
         placement: placements[index]
       }));
-
+      
       heat.results = results;
       await heat.save();
-
-      // Update each racer's total points.
-      // Using points logic: points = (number of racers in heat) - (placement - 1)
+      
+      // Points assignment logic: points = numRacers - (placement - 1)
       for (const result of results) {
         const pointsAwarded = numRacers - (result.placement - 1);
         await Racer.findByIdAndUpdate(result.racer, { $inc: { points: pointsAwarded } });
