a04ab8d693ab8ff6691f5bc93ef48c9001a5ca9b
hmustain <mustain05@aim.com>
2025-03-13 20:05:19 -0500

    updated basic landing page structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,14 @@
+// App.jsx
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Registration from "./pages/Registration";
-// import HeatRaces from "./pages/HeatRaces";
-// import Bracket from "./pages/Bracket";
+import LandingPage from "./components/LandingPage";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Registration />} />
-        <Route path="/heat-races" element={<HeatRaces />} />
-        <Route path="/bracket" element={<Bracket />} />
+        <Route path="/" element={<LandingPage />} />
+        {/* other routes as needed */}
       </Routes>
     </Router>
   );
--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import './LandingPage.css';
+import { Link } from 'react-router-dom';
+import '../styles/LandingPage.css';
 
-const LandingPage = () => {
+function LandingPage() {
   return (
-    <div className="landing-container">
-      <h1>Welcome to the Grand Prix!</h1>
+    <div className="landing-container d-flex flex-column justify-content-center align-items-center text-center min-vh-100 text-white">
+      <h1 className="display-1">Welcome to Awana Grand Prix!</h1>
+      
+      {/* Single call-to-action button with a more contrasting color scheme */}
+      <Link to="/registration" className="btn btn-dark btn-lg mt-4 px-4 py-3">
+        Get Started!
+      </Link>
     </div>
   );
-};
+}
 
 export default LandingPage;
