b5286867bce3567043e0f7fefd335aaa215675e2
hmustain <mustain05@aim.com>
2025-03-13 19:19:56 -0500

    working on adding landingpage setup with background image

---
+++ client/src/components/LandingPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './LandingPage.css';
+
+const LandingPage = () => {
+  return (
+    <div className="landing-container">
+      <h1>Welcome to the Grand Prix!</h1>
+    </div>
+  );
+};
+
+export default LandingPage;
